ddb8f000ab2bfc6504bd2d80101a5f37536f161c
tkurudi <t.kurudi209@gmail.com>
2019-04-19 18:37:54 +1200

    firday

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,6 +1,9 @@
-import React, {Fragment } from 'react'
+import React, {Fragment, useState } from 'react'
 
 const FileUpload = () => {
+    const [ file, setFile] = useState('');
+    const [ filename, setFilename] = useState('Choose name');
+   
   return (
     
         <Fragment>
