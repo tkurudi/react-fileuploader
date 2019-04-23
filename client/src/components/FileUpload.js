1359ca6d0e2d7b2c5d629ddd9597f46913321ed7
tkurudi <t.kurudi209@gmail.com>
2019-04-23 20:19:55 +1200

    more stuff

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -2,15 +2,29 @@ import React, {Fragment, useState } from 'react'
 
 const FileUpload = () => {
     const [ file, setFile] = useState('');
-    const [ filename, setFilename] = useState('Choose name');
+    const [ filename, setFilename] = useState('Choose File');
+
+    const onChange = e => {
+        setFile(e.target.files[0]);
+        setFilename(e.target.files[0].name);
+
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+    }
    
   return (
     
         <Fragment>
-        <form>
+        <form onSubmit={onSubmit}>
          <div className="custom-file mb-4">
-            <input type="file" className="custom-file-input" id="customFile" />
-            <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+            <input type="file" className="custom-file-input" id="customFile" onChange={onChange} />
+            <label className="custom-file-label" htmlFor="customFile">
+            {filename}
+            </label>
          </div>
           <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4 "/>
         </form>
